ff6bde0c2dd56514d62e05ddeb7fb2c9061949bb
Ivana Kowalczuk <88699630+Ivanakowalczuk@users.noreply.github.com>
2023-10-09 13:32:06 -0300

    Update page.tsx
    cambios en grillProyectos al llamar a getData

--- components/grillaProyectos/page.tsx
+++ components/grillaProyectos/page.tsx
@@ -1,10 +1,13 @@
+'use client'
 import React from "react";
 import Card from "../card/page";
+import { useEffect, useState} from "react";
 
+const [proyectos, setProyectos] = useState([])
 const getData = async () => {
     try {
       const response = await fetch(`${process.env.URI}`, { cache: "no-store" });
-    
+     setProyectos(response.json)
       return response.json();
     } catch (error) {
       console.log("Error : ", error);
@@ -12,8 +15,10 @@ const getData = async () => {
   };
 
   const GrillaProyectos = async () => {
-    const {data} = await getData()
-  
+    //const {data} = await getData()
+  useEffect(()=> {
+   getData() 
+  }, [])
     return(
       <div className="grid grid-cols-1 lg:grid-cols-2 gap-4 p-2">
         {data.map((elem: any)=>(
