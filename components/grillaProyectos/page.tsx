740caae16a2c172f330c6d5e65ed3579008e2f68
Ivanakowalczuk <ivanakowalczuk@gmail.com>
2023-10-09 17:03:00 -0300

    cambios2

--- components/grillaProyectos/page.tsx
+++ components/grillaProyectos/page.tsx
@@ -1,13 +1,12 @@
-'use client'
 import React from "react";
 import Card from "../card/page";
-import { useEffect, useState} from "react";
 
-const [proyectos, setProyectos] = useState([])
+
+
 const getData = async () => {
     try {
       const response = await fetch(`${process.env.URI}`, { cache: "no-store" });
-     setProyectos(response.json)
+  
       return response.json();
     } catch (error) {
       console.log("Error : ", error);
@@ -15,13 +14,11 @@ const getData = async () => {
   };
 
   const GrillaProyectos = async () => {
-    //const {data} = await getData()
-  useEffect(()=> {
-   getData() 
-  }, [])
+    const {data} = await getData()
+
     return(
       <div className="grid grid-cols-1 lg:grid-cols-2 gap-4 p-2">
-        {proyectos?.map((elem: any)=>(
+        {data.map((elem: any)=>(
           <>
          
           <Card data={elem}/>
